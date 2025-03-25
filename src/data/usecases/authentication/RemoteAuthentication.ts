b19abd226f99b7dc8437a64340a837db1722a99a
rod <dev_rodts@outlook.com>
2025-03-25 07:18:29 -0300

    feat: ensure remote authentication calls http post client

---
+++ src/data/usecases/authentication/RemoteAuthentication.ts
@@ -0,0 +1,11 @@
+import { HttpPostClient } from '../../protocols/http/HttpPostClient';
+
+export class RemoteAuthentication{
+    constructor(private readonly url: string, private readonly httpPostClient: HttpPostClient){
+    }
+    async auth(): Promise<void>{
+        await this.httpPostClient.post(this.url);
+        return Promise.resolve();
+    }
+}
+
